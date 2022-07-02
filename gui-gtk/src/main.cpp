db01072ccb0e767833dedef40c479896d0f461eb
AdamYuan <y13916619121@126.com>
2022-07-02 12:18:33 +0800

    GTK setup

---
+++ gui-gtk/src/main.cpp
@@ -0,0 +1,8 @@
+#include <gtkmm.h>
+
+int main(int argc, char *argv[]) {
+	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
+
+	Gtk::ApplicationWindow window;
+	return app->run(window);
+}
